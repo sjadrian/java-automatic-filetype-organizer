1298c12ee8c7f129ffee9175c1321f09c5c91aef
Stefanus Adrian <stefanusj06@gmail.com>
2023-07-31 15:38:16 +0700

    added FileSorter.Java and updated Main.java

---
+++ src/main/java/com/sjadrian/FileSorter.java
@@ -0,0 +1,60 @@
+package com.sjadrian;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class FileSorter {
+
+    private final String downloadPath;
+
+    public FileSorter() {
+       this.downloadPath  = getDownloadDirectoryData();
+    }
+
+    private static String getDownloadDirectoryData() {
+        Properties properties = new Properties();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("config"))) {
+            properties.load(reader);
+        }  catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties.get("DOWNLOAD_PATH").toString();
+    }
+
+    public void run() throws IOException {
+        // Check Download Path
+        System.out.println("Download path = " + downloadPath);
+
+        // Move each file based on its file type
+        Files.list(Paths.get(downloadPath)).filter(obj-> obj.toFile().isFile()).forEach(obj-> move(obj.toFile().getName()));
+    }
+
+    private void move(String fileName) {
+        // get file extension
+        String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
+
+        // get the target directory path
+        String targetPathString = downloadPath + "\\" + fileExtension + "\\";
+        Path targetPath = Paths.get(targetPathString);
+
+        // check if directory exist
+        if (!Files.exists(targetPath)) {
+            try {
+                Files.createDirectory((targetPath));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try {
+            // move the file to its designated folder based on each type
+            Path temp = Files.move(Paths.get(downloadPath + fileName), Paths.get(targetPathString  + fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/sjadrian/Main.java
+++ src/main/java/com/sjadrian/Main.java
@@ -1,56 +1,11 @@
 package com.sjadrian;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Properties;
 
 public class Main {
 
-    private static final String downloadPath =  getDownloadDirectoryData();
-
-    private static String getDownloadDirectoryData() {
-
-        Properties properties = new Properties();
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get("config"))) {
-            properties.load(reader);
-        }  catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return properties.get("DOWNLOAD_PATH").toString();
-    }
-
     public static void main(String[] args) throws IOException {
-        // Check Download Path
-        System.out.println("Download path = " + downloadPath);
-
-        // Move each file based on its file type
-        Files.list(Paths.get(downloadPath)).filter(obj-> obj.toFile().isFile()).forEach(obj-> move(obj.toFile().getName()));
-    }
-
-    private static void move(String fileName) {
-        // get file extension
-        String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
-
-        // get the target directory path
-        String targetPathString = downloadPath + "\\" + fileExtension + "\\";
-        Path targetPath = Paths.get(targetPathString);
-
-        // check if directory exist
-        if (!Files.exists(targetPath)) {
-            try {
-                Files.createDirectory((targetPath));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        try {
-            // move the file to its designated folder based on each type
-            Path temp = Files.move(Paths.get(downloadPath + fileName), Paths.get(targetPathString  + fileName));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        FileSorter fileSorter = new FileSorter();
+        fileSorter.run();
     }
 }
