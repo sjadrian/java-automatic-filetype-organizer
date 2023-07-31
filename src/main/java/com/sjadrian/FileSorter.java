663404aa9db566b6be991633b2de6bb183bde2fe
Stefanus Adrian <stefanusj06@gmail.com>
2023-07-31 22:54:43 +0700

    updated FileSorter.java

--- src/main/java/com/sjadrian/FileSorter.java
+++ src/main/java/com/sjadrian/FileSorter.java
@@ -3,7 +3,6 @@ package com.sjadrian;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
--- src/main/java/com/sjadrian/Main.java
+++ src/main/java/com/sjadrian/Main.java
@@ -1,10 +1,7 @@
 package com.sjadrian;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Properties;
+import java.io.IOException;
+
 
 public class Main {
 
